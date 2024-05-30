1e58c2859da0218d0cba4b37648133afc4742372
Josemaria-De-Alba <142268975+Josemaria-De-Alba@users.noreply.github.com>
2024-05-30 07:40:22 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -16,27 +16,24 @@ int main() {
   //Todos estos son para probar que funcionen las funciones
   Weapon gun{};
   gun.print_stats();
-  Weapon gun2("Shotgun", "Short Range");
-  gun2.print_stats();
-  Weapon gun3("Sniper", "Long Range", 5, true, "Rare");
-  gun3.print_stats();
-
-  Exotic exotic_default{};
-  exotic_default.print_stats();
-
-  Exotic final_warning("Final Warning", "Sidearm", 20, true ,"Pick Your Poison");
-  final_warning.print_stats();
-
+  
   Exotic quicksilver("Quicksilver", "Auto-Rifle", 50, true,"Rocket Tracers", "Nano-Entanglement");
   quicksilver.print_stats();
 
-  Legendary legendary_default{};
-  legendary_default.print_stats();
+  Legendary ikelos_sg("IKELOS_SG_v1.0.3", "Shotgun", 8, false, "Rasputins Arsenal", "Reload");
+  ikelos_sg.print_stats();
+  
+  //Pruebas Polimorfismo
+  Weapon *poliweapon = new Weapon();
+  poliweapon->print_stats();
+  poliweapon->characteristics();
+  
+  Weapon *navigator = new Exotic("Protective Weave","Not Found");
+  navigator->print_stats();
+  navigator->setName("Navigator");
+  navigator->characteristics();
 
-  Legendary mountaintop("Mountaintop", "Grenade Launcher", 1, false, "Indomitability");
-  mountaintop.print_stats();
-
-  Legendary iklos_sg("IKELOS_SG_v1.0.3", "Shotgun", 8, false, "Rasputins Arsenal", "Reload");
-  iklos_sg.print_stats();
-
+  Weapon *indebted_kindness = new Legendary("Indebted Kindness", "Rocket-Assisted Frame", "Stability");
+  indebted_kindness->print_stats();
+  indebted_kindness->characteristics();
 }
